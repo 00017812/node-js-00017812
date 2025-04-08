ea01261977ecd85f2acc350df499b6abcfc1d0b9
00017812 <00017812wiut@gmail.com>
2025-04-08 21:19:36 +0500

    DEbugging but not finding issue

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const path = require('path');
 
-const apiRoutes = require('./routes/api');
-const webRoutes = require('./routes/web');
+const vocabularyRouter = require('./routes/vocabulary'); 
+const webRoutes = require('./routes/web'); 
 
-const port = 3000
+const port = 3000;
 
 const app = express();
 
 global.mock_db = path.join(__dirname, './data/mock_db.json');
 
-// Middleware to parse JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set Pug as the view engine and point to the views folder
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use('/images', express.static(path.join(__dirname, 'views/vocabulary/images')));
 
-// Serve static files (for CSS/JS)
+app.get('/vocabulary', (req, res) => {
+    res.render('vocabulary/index', { vocabularies });
+});
+
 app.use('/css', express.static('public/css'));
 app.use('/js', express.static('public/js'));
 
-// Use API and web routes
-app.use('/api', apiRoutes);
+app.use('/api', vocabularyRouter); // Change this if you're using a different router
 app.use('/', webRoutes);
 
 app.listen(port, () => {
--- routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const vocabularyRouter = require('./vocabulary')
-
-const router = express.Router()
-
-router.use('/vocabulary', vocabularyRouter)
-
-module.exports = router
